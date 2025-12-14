4ced68a46d877aa89173a8922c06e47eca002f21
Mohit Anuragi <154867637+MohitAnuragi@users.noreply.github.com>
2025-12-15 04:00:54 +0530

    Update PermissionResult.kt

--- permissionkit/src/main/java/com/permissionkit/permissionkit/PermissionResult.kt
+++ permissionkit/src/main/java/com/permissionkit/permissionkit/PermissionResult.kt
@@ -1,21 +1,10 @@
 package com.permissionkit.permissionkit
 
-/**
- * Represents the result of a permission request.
- */
-sealed class PermissionResult {
-    /**
-     * Permission was granted by the user.
-     */
-    object Granted : PermissionResult()
 
-    /**
-     * Permission was denied by the user.
-     */
+sealed class PermissionResult {
+    object Granted : PermissionResult()
     data class Denied(
-        /**
-         * True if the user checked "Don't ask again" or if this is a system-level denial.
-         */
+     
         val shouldShowRationale: Boolean
     ) : PermissionResult()
 }
